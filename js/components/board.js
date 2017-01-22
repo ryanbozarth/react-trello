eace07620cb5632c4fe3fb8aac6dafda876f43d9
Ryan Bozarth <ryan.bozarth@gmail.com>
2017-01-22 10:05:41 -0800

    created props for board and list titles

--- js/components/board.js
+++ js/components/board.js
@@ -2,15 +2,24 @@ import React from 'react';
 import CardList from './list';
 
 export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Board One",
+      list: ["List One", "List Two", "List Three"]
+    }
+  }
+  renderCardList() {
+    return this.state.list.map(function(title, i){
+      return <CardList key={i} title={title} />
+    })
+  }
   render() {
-    const board = [];
-      for (let i=0; i<3; i++) {
-          board.push(<CardList />);
-      }
-      return (
-          <div className="board-list">
-              {board}
-          </div>
-      );
+    return (
+        <div className="board-list">
+          <h1>{this.state.title}</h1>
+          {this.renderCardList()}
+        </div>
+    );
   }
 }
--- js/components/list.js
+++ js/components/list.js
@@ -2,14 +2,21 @@ import React from 'react';
 import Card from './card';
 
 export default class CardList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title
+    }
+  }
   render() {
     const card = [];
       for (let i=0; i<3; i++) {
-          card.push(<Card />);
+          card.push(<Card key={i} />);
       }
       return (
           <div className="card-list">
-              {card}
+            <span>{this.state.title}</span>
+            {card}
           </div>
       );
   }
